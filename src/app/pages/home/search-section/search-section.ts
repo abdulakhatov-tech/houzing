cf6c2b8a19a7b5483557a0291d88307eb31097d9
abdulakhatov-tech <islomabdulakhatov6975@gmail.com>
2025-09-18 21:35:29 +0500

    feat: add search section

--- src/app/pages/home/search-section/search-section.ts
+++ src/app/pages/home/search-section/search-section.ts
@@ -1,25 +1,33 @@
-import { ActivatedRoute, Router } from '@angular/router'; 
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { ZardButtonComponent } from '@shared/components/button/button.component';
 import { ZardInputDirective } from '@shared/components/input/input.directive';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, filter } from 'rxjs/operators';
 import {
   ZardPopoverComponent,
   ZardPopoverDirective,
 } from '@shared/components/popover/popover.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-section',
   standalone: true,
-  imports: [ReactiveFormsModule, ZardInputDirective, ZardButtonComponent, ZardPopoverComponent, ZardPopoverDirective],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ZardInputDirective,
+    ZardButtonComponent,
+    ZardPopoverComponent,
+    ZardPopoverDirective,
+  ],
   template: `
     <section id="search-section" class="py-[10px]">
       <div class="container relative">
         <form [formGroup]="searchForm" class="flex items-center gap-2 md:gap-4">
           <input
             z-input
-             formControlName="keyword"
+            formControlName="keyword"
             placeholder="Enter an address, city, or ZIP code"
             class="flex-1 px-4 h-10"
           />
@@ -36,31 +44,71 @@ import {
           </button>
 
           <ng-template #popoverContent>
-            <z-popover class="w-[90vw] sm:w-[92vw] lg:w-[900px] absolute -right-[80px] sm:-right-[170px] md:-right-[265px] top-2">
+            <z-popover
+              class="w-[90vw] sm:w-[92vw] lg:w-[900px] absolute -right-[80px] sm:-right-[170px] md:-right-[265px] top-2"
+            >
               <div class="space-y-2">
                 <h4 class="font-medium leading-none">Address</h4>
                 <div class="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-3">
-                  <input z-input placeholder="Country" formControlName="country" class="px-3 md:px-4 h-10" />
-                  <input z-input placeholder="Region" formControlName="region" class="px-3 md:px-4 h-10" />
-                  <input z-input placeholder="City" formControlName="city" class="px-3 md:px-4 h-10" />
-                  <input z-input placeholder="Zip code" formControlName="zip" class="px-3 md:px-4 h-10" />
+                  <input
+                    z-input
+                    placeholder="Country"
+                    formControlName="country"
+                    class="px-3 md:px-4 h-10"
+                  />
+                  <input
+                    z-input
+                    placeholder="Region"
+                    formControlName="region"
+                    class="px-3 md:px-4 h-10"
+                  />
+                  <input
+                    z-input
+                    placeholder="City"
+                    formControlName="city"
+                    class="px-3 md:px-4 h-10"
+                  />
+                  <input
+                    z-input
+                    placeholder="Zip code"
+                    formControlName="zip"
+                    class="px-3 md:px-4 h-10"
+                  />
                 </div>
 
                 <h4 class="font-medium leading-none mt-3 md:mt-5">Apartment Info</h4>
                 <div class="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-3">
                   <input z-input placeholder="Rooms" formControlName="rooms" class="px-4 h-10" />
                   <input z-input placeholder="Size" formControlName="size" class="px-4 h-10" />
-                  <input z-input placeholder="Sort" formControlName="sort" class="px-4 h-10"/>
+                  <input z-input placeholder="Sort" formControlName="sort" class="px-4 h-10" />
                 </div>
 
                 <h4 class="font-medium leading-none mt-3 md:mt-5">Price</h4>
                 <div class="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-3">
-                  <input z-input placeholder="Min price" formControlName="minPrice" class="px-3 md:px-4 h-10" />
-                  <input z-input placeholder="Max price" formControlName="maxPrice" class="px-4 h-10" />
+                  <input
+                    z-input
+                    placeholder="Min price"
+                    formControlName="minPrice"
+                    class="px-3 md:px-4 h-10"
+                  />
+                  <input
+                    z-input
+                    placeholder="Max price"
+                    formControlName="maxPrice"
+                    class="px-4 h-10"
+                  />
                 </div>
 
                 <div class="flex items-center justify-end gap-3 mt-5 md:mt-7">
-                  <button z-button zType="destructive" class="flex-1 md:w-30" (click)="resetParams()">Clear</button>
+                  <button
+                    z-button
+                    zType="destructive"
+                    class="flex-1 md:w-30"
+                    *ngIf="hasValues()"
+                    (click)="resetParams()"
+                  >
+                    Clear
+                  </button>
                   <!-- <button z-button zType="default" class="flex-1 md:w-30 bg-secondary-blue hover:bg-secondary-blue/90">Submit</button> -->
                 </div>
               </div>
@@ -70,7 +118,6 @@ import {
           <button
             z-button
             class="bg-secondary-blue h-10 sm:w-fit md:w-45 hover:bg-secondary-blue/90"
-           
           >
             <img src="/assets/icons/search.svg" alt="Search" />
             <span class="hidden sm:block">Search</span>
@@ -84,8 +131,7 @@ import {
 export class SearchSection {
   searchForm!: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router, 
-    private route: ActivatedRoute) {
+  constructor(private fb: FormBuilder, private router: Router, private route: ActivatedRoute) {
     this.searchForm = this.fb.group({
       keyword: [''],
       country: [''],
@@ -105,20 +151,23 @@ export class SearchSection {
     this.route.queryParams.subscribe((params) => {
       this.searchForm.patchValue(params, { emitEvent: false });
     });
-
+  
     // 2. Listen to form changes and update query params
-    this.searchForm.valueChanges
-      .pipe(debounceTime(400), distinctUntilChanged())
-      .subscribe((values) => {
-        this.router.navigate(['/properties'], {
-          queryParams: this.cleanParams(values),
-          queryParamsHandling: 'merge',
-        });
+    this.searchForm.valueChanges.pipe(debounceTime(400)).subscribe((values) => {
+      this.router.navigate(['/properties'], {
+        queryParams: this.cleanParams(values), // always replace params
       });
+    });
+  
+    // 3. Reset form when leaving /properties
+    this.router.events.pipe(filter((e) => e instanceof NavigationEnd)).subscribe((event: any) => {
+      if (!event.url.startsWith('/properties')) {
+        this.searchForm.reset({}, { emitEvent: false });
+      }
+    });
   }
 
   private cleanParams(values: any) {
-    // remove empty fields from query params
     return Object.fromEntries(
       Object.entries(values).filter(([_, v]) => v !== null && v !== '')
     );
@@ -129,4 +178,7 @@ export class SearchSection {
     this.router.navigate([], { queryParams: {} });
   }
 
+  hasValues(): boolean {
+    return Object.values(this.searchForm.value).some((v) => v && v !== '');
+  }
 }
